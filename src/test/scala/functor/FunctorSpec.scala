f4bf372b4ff9e186de3fef83bc9a19eea97bbc53
Matthew Pagan <mastapegs01@gmail.com>
2024-08-22 14:04:44 -0400

    actual tests for functor

--- src/test/scala/functor/FunctorSpec.scala
+++ src/test/scala/functor/FunctorSpec.scala
@@ -1,7 +1,18 @@
 package functor
 
 class FunctorSpec extends munit.FunSuite {
-  test("any assertion") {
-    assert(1 == 1)
+  import Functor._
+
+  case class TestClass[A](value: A)
+  implicit val TestClassFunctor: Functor[TestClass] = new Functor[TestClass] {
+    def map[A, B](fa: TestClass[A])(f: A => B): TestClass[B] = fa match {
+      case TestClass(value) => TestClass(f(value))
+    }
+  }
+
+  test("functors can map") {
+    val obtained = TestClass(3).map(_ * 2)
+    val expected = TestClass(6)
+    assertEquals(obtained, expected)
   }
 }
