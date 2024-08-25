f1b4b500a3743bd9900f3d7a7f426d1357ea9584
Matthew Pagan <mastapegs01@gmail.com>
2024-08-25 19:51:12 -0400

    create nice apply for semigroup

--- src/main/scala/applicative/Applicative.scala
+++ src/main/scala/applicative/Applicative.scala
@@ -9,11 +9,12 @@ trait Applicative[F[_]] extends Functor[F] {
 }
 
 object Applicative {
+  def apply[F[_]: Applicative]: Applicative[F] = implicitly[Applicative[F]]
   def ap[F[_]: Applicative, A, B](ff: F[A => B])(fa: F[A]): F[B] =
-    implicitly[Applicative[F]].ap(ff)(fa)
+    Applicative[F].ap(ff)(fa)
 
   def pure[F[_]: Applicative, A](a: A) =
-    implicitly[Applicative[F]].pure(a)
+    Applicative[F].pure(a)
 
   implicit class ApplicativeOpsAp[F[_]: Applicative, A, B](ff: F[A => B]) {
     def <*>(fa: F[A]): F[B] = ap(ff)(fa)
